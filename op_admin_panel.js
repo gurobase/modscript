001bef806feeb34c6a5d1701380b44ff98bb345d
Guro <10135009+gurobase@users.noreply.github.com>
2022-12-06 23:45:04 +0200

    Create op_admin_panel.js

---
+++ op_admin_panel.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         OP Admin Panel
+// @version      1.0.0
+// @description  For when you need just a lil' bit more juice
+// @author       Guro
+// @match        https://control.stripchat.com/new/photos/moderation
+// @icon         https://control.my.club/favicon.png
+// @grant        GM_addStyle
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
+// @grant        GM.xmlHttpRequest
+// @downloadURL  https://raw.githubusercontent.com/gurobase/modscript/main/op_admin_panel.js
+
+// ==/UserScript==
